53ca306818b1c15c67dd57f8d10bc609bfbae49b
bilalanees98 <bilalanees1998@gmail.com>
2023-04-10 17:26:37 +0500

    Updates

---
+++ _app/immutable/chunks/1.e53b6142.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.4f417411.js";export{t as component};
---
+++ _app/immutable/chunks/2.bdbd0b93.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.40f003f8.js";export{t as component};
